c40cb146034f9bcef8d2c8ceedb9021e2b51780c
Gisbert Amm <gisbert.amm@web.de>
2015-07-14 14:39:56 +0200

    created a new Xcode project

---
+++ Kfz-Kennzeichen/Kfz-Kennzeichen/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Kfz-Kennzeichen
+//
+//  Created by Gisbert Amm on 14.07.15.
+//  Copyright (c) 2015 Gisbert Amm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
